684353dc70828ecd2141ee0ff9f6a840864a315b
Theo Hancock <theo.hancock98@gmail.com>
2023-10-17 13:57:28 +0800

    Add project files.

---
+++ WebDBServer/Models/ClientListModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebDBServer.Models
+{
+    public static class ClientListModel
+    {
+        private static List<ClientModel>  Clients { get; } = new List<ClientModel>();
+    }
+}
---
+++ WebDBServer/Models/ClientModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDBServer.Models
+{
+    public class ClientModel
+    {
+        public int ID { get; set; } // Might not be needed 
+        public string? IP { get; set; }
+        public int Port { get; set; }
+    }
+}
